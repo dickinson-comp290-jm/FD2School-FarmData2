c16a49cff79c8b988b8c0a97891de2e9d3714221
braught <braught@dickinson.edu>
2023-03-06 21:56:10 -0500

    Added E2E test for the Harvest Report start and end dates

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -8,5 +8,12 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=page-header]")
             .should("have.text","Harvest Report")
     })
+
+    it("Check the default start and end dates", () => {
+        cy.get("[data-cy=start-date]")
+            .should("have.value","2020-05-05")
+        cy.get("[data-cy=end-date]")
+            .should("have.value","2020-05-15")
+    })
 })
     
