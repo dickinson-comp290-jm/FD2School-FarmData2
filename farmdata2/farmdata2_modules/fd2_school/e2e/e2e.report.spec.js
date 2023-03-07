3a6200c4b65fc90e3ec91074385b4e61d9aec0be
braught <braught@dickinson.edu>
2023-03-07 14:23:58 -0500

    Added E2E test to check harvest report Details (farm, user, language)

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
@@ -9,4 +9,11 @@ describe("Test the generation of the harvest report", () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=report-header]").should("be.visible")
     })
+
+    it("Check that details are correct when report is generated", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]").should("have.text", "Farm: Sample Farm")
+        cy.get("[data-cy=username]").should("contain.text", "manager1")
+        cy.get("[data-cy=language]").should("have.text", "English")
+    })
 })
