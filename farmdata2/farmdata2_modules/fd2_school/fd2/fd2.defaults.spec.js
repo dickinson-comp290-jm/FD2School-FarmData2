24a4b539ca1e93d7dabcd1be5ca7907b81810d5f
johnmaccormick <jmac@dickinson.edu>
2023-03-21 22:00:52 -0400

    created new FD2 subtab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -0,0 +1,28 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+    })
+      
+    it("Check the page header", () => {
+        cy.get("[data-cy=page-header]")
+            .should("have.text","Harvest Report")
+    })
+
+    it("Check the default start and end dates", () => {
+        cy.get("[data-cy=start-date]")
+            .should("have.value","2020-05-05")
+        cy.get("[data-cy=end-date]")
+            .should("have.value","2020-05-15")
+    })
+
+    it("Check the crops in the crop drop down", () => {
+        let list = cy.get("[data-cy=crop-dropdown]").children()
+        
+        cy.get("[data-cy=crop-dropdown]").children().eq(0).should("have.text","ARUGULA")
+        cy.get("[data-cy=crop-dropdown]").children().eq(4).should("have.text","BEAN-FAVA")
+        cy.get("[data-cy=crop-dropdown]").children().eq(110).should("have.text","ZUCCHINI")
+        cy.get("[data-cy=crop-dropdown]").children().should("have.length", 111)
+    })
+})
+    
