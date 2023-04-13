d8d04c3b650fdab6d1835536f1e473af132c71cb
JinLeeGG <leejo@dickinson.edu>
2023-04-13 16:13:02 -0400

    changed url for beforeEach and sub-task1 finished

--- farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldkit.tab.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldkit.tab.spec.js
@@ -2,7 +2,13 @@ describe('Test for FieldKit Sub-Tabs', () => {
 
   beforeEach(() => {
     cy.login('manager1', 'farmdata2')
-    cy.visit('/farm/fd2-field-kit/seedingInput')
+    cy.visit('/farm/fd2-field-kit')
+  })
+
+  //issue #200, sub-task 1
+  it('verify that FieldKit tab contains sub-tabs for "Info" and "Seeding Input"', () =>{
+    cy.get('.pagination-sm').contains('Info').should('exist')
+    cy.get('.pagination-sm').contains('Seeding Input').should('exist')
   })
 
 })
