cdb434b1451a5b34d50ab6111be4a434aba2f4e2
JinLeeGG <leejo@dickinson.edu>
2023-04-13 15:19:29 -0400

    beforeEach added for login in farmdata2 web

--- farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldkit.tab.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldkit.tab.spec.js
@@ -1,5 +1,8 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test for FieldKit Sub-Tabs', () => {
+
+  beforeEach(() => {
+    cy.login('manager1', 'farmdata2')
+    cy.visit('/farm/fd2-field-kit/seedingInput')
   })
+
 })
