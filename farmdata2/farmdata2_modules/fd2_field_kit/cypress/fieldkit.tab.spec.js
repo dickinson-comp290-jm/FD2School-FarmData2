c6e76d35640dd3e6c949a3d4000207d55f3f7bfe
won369369 <wj0293@gmail.com>
2023-04-19 00:43:10 -0400

    Implemented a test that checks the correct number of sub-tabs

--- farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldkit.tab.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldkit.tab.spec.js
@@ -6,17 +6,26 @@ describe('Test for FieldKit Sub-Tabs', () => {
   })
 
   //issue #200, sub-task 1
-  it('Verify that FieldKit tab contains sub-tabs for "Info" and "Seeding Input"', () =>{
+  it('Verify that FieldKit tab contains sub-tabs for "Info" and "Seeding Input"', () => {
     cy.get('.pagination-sm').contains('Info').should('exist')
     cy.get('.pagination-sm').contains('Seeding Input').should('exist')
   })
 
   //issue #200, sub-task 2
-  it('checks the order of the tabs is “Info” and then “Seeding Input.”', () =>{
+  it('checks the order of the tabs is “Info” and then “Seeding Input.”', () => {
     cy.get('ul.pagination-sm')
       .find('li')
       .filter(':contains("Info")')
       .next()
       .should('contain', 'Seeding Input');
   })
+
+  //issue #200, sub-task 3
+  it('checks the correct number of sub-tabs', () => {
+    cy.get('ul.pagination-sm')
+      .find('li')
+      .filter(':contains("Info"), :contains("Seeding Input")')
+      .should('have.length', 2)
+  })
+
 })
